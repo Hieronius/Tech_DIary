f0a5e581c2828f008def164d9d24726128a6fe47
Arsenty Halimovsky <ghost12@yandex.ru>
2024-09-29 03:42:09 +0300

    add comments to Category

--- main.swift
+++ main.swift
@@ -1,13 +1,12 @@
 // MARK: - You Compiler can see only main.swift file on the current settings
 
-
-/*  MARK: High Order Functions
-
- */
 // Keep the main thread alive to see the output
 // RunLoop.main.run()
 
 import Foundation
 
 var array = [38, 27, 43, 3, 9, 82, 10]
-// print(mergeSort(array))
+//var array = [3, 10, 5, 55, 78, 1, 2, 12, 100]
+//print(mergeSort(array))
+
+
